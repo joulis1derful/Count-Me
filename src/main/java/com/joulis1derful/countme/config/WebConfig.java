1ab9bcc6c5645dcddc21e76d92e24dcb489a531e
joulis1derful <joulis1derful@gmail.com>
2017-08-01 23:12:21 +0300

    Added ViewResolver

--- src/main/java/com/joulis1derful/countme/config/WebConfig.java
+++ src/main/java/com/joulis1derful/countme/config/WebConfig.java
@@ -1,16 +1,22 @@
 package com.joulis1derful.countme.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.List;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.joulis1derful.countme")
 public class WebConfig extends WebMvcConfigurerAdapter {
-
+    @Bean
+    public ViewResolver getViewResolver(){
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 }
